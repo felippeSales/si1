052eb97907970967fdd8ff3a8773adf17e71c250
Felipe Sales <sales.felipeb@gmail.com>
2013-11-06 17:31:20 -0300

    Teste de 0 até 10

--- lab01/lab01/src/PorExtenso/PorExtenso.java
+++ lab01/lab01/src/PorExtenso/PorExtenso.java
@@ -1,14 +1,30 @@
 package PorExtenso;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 
 public class PorExtenso {
 
-	public String transformaExtenso(int i) {
+	public String transformaExtenso(String i) {
+		
+		Hashtable<String, String> numeros = new Hashtable<String, String>();
+		
+		numeros.put("0", "zero");
+		numeros.put("1", "um");
+		numeros.put("2", "dois");
+		numeros.put("3", "tres");
+		numeros.put("4", "quatro");
+		numeros.put("5", "cinco");
+		numeros.put("6", "seis");
+		numeros.put("7", "sete");
+		numeros.put("8", "oito");
+		numeros.put("9", "nove");
+		numeros.put("10", "dez");
+		
 		String resultado = "";
 		
-		if(i == 0){
-			resultado += "zero";
-		}
+		resultado += numeros.get(i);
 		
 		return resultado;
 	}
--- lab01/lab01/src/PorExtenso/TestPorExtenso.java
+++ lab01/lab01/src/PorExtenso/TestPorExtenso.java
@@ -1,17 +1,35 @@
 package PorExtenso;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class TestPorExtenso {
 
 	@Test
-	public void test(){
+	public void test0(){
 		
 		PorExtenso transformador = new PorExtenso();
 		
-		assertEquals(transformador.transformaExtenso(0), "zero");
+		assertEquals(transformador.transformaExtenso("0"), "zero");
+		
+	}
+	
+	@Test
+	public void testAte10(){
+		
+		PorExtenso transformador = new PorExtenso();
+		
+		assertEquals(transformador.transformaExtenso("1"), "um");
+		assertEquals(transformador.transformaExtenso("2"), "dois");
+		assertEquals(transformador.transformaExtenso("3"), "tres");
+		assertEquals(transformador.transformaExtenso("4"), "quatro");
+		assertEquals(transformador.transformaExtenso("5"), "cinco");
+		assertEquals(transformador.transformaExtenso("6"), "seis");
+		assertEquals(transformador.transformaExtenso("7"), "sete");
+		assertEquals(transformador.transformaExtenso("8"), "oito");
+		assertEquals(transformador.transformaExtenso("9"), "nove");
+		assertEquals(transformador.transformaExtenso("10"), "dez");
 		
 	}
 
