2542be699a38c10dc8236b178c4e54ac15e21618
Felipe Sales <sales.felipeb@gmail.com>
2013-11-08 01:30:26 -0300

    Teste todos os formados por uma única palavra, quando escritos por extenso

--- lab01/lab01/src/PorExtenso/PorExtenso.java
+++ lab01/lab01/src/PorExtenso/PorExtenso.java
@@ -6,18 +6,38 @@ import java.util.Hashtable;
 
 public class PorExtenso {
 
-	public String transformaExtenso(String i) {
+	public String transformaExtenso(String numero) {
 		
 		Hashtable<String, String> listaNumeros = new Hashtable<String, String>();
-		String[] numeros = {"zero","um", "dois","tres", "quatro","cinco","seis","sete","oito","nove", "dez"}; 
 		
-		for (int j = 0; j < numeros.length; j++) {
+		String[] numeros = {"zero","um", "dois","tres", "quatro","cinco","seis","sete","oito"
+				,"nove", "dez","onze","doze", "treze", "catorze", "quinze", "dezesseis", "dezessete"
+				,"dezoito", "dezenove", "vinte","trinta","quarenta","cinquenta", "sessenta",
+				"setenta", "oitenta", "noventa", "cem","duzentos", "trezentos", "quatrocentos",
+				"quinhentos", "seiscentos", "setecentos","oitocentos", "novecentos", "mil"}; 
+		
+		
+		for (int j = 0; j < 21; j++) {
 			listaNumeros.put(j+"", numeros[j]);
+		}
+		
+		int contador = 30;
+		
+		for(int n = 21; n < 29; n++){
+			listaNumeros.put(contador+"", numeros[n]);
+			contador += 10;
+		}
+		
+		contador = 100;
+		for(int n2 = 29; n2 < 38; n2++){
+			contador += 100;
+			
+			listaNumeros.put(contador+"", numeros[n2]);
 		}
 						
 		String resultado = "";
 		
-		resultado += listaNumeros.get(i);
+		resultado += listaNumeros.get(numero);
 		
 		return resultado;
 	}
--- lab01/lab01/src/PorExtenso/TestPorExtenso.java
+++ lab01/lab01/src/PorExtenso/TestPorExtenso.java
@@ -2,14 +2,20 @@ package PorExtenso;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestPorExtenso {
 
+	PorExtenso transformador;
+	
+	@Before
+	public void setUP(){
+		transformador = new PorExtenso();
+	}
+	
 	@Test
 	public void test0(){
-		
-		PorExtenso transformador = new PorExtenso();
 		
 		assertEquals(transformador.transformaExtenso("0"), "zero");
 		
@@ -17,9 +23,7 @@ public class TestPorExtenso {
 	
 	@Test
 	public void testAte10(){
-		
-		PorExtenso transformador = new PorExtenso();
-		
+	
 		String[] numeros = {"zero","um", "dois","tres", "quatro","cinco","seis","sete","oito","nove", "dez"};
 		
 		for (int i = 1; i < numeros.length; i++) {
@@ -28,6 +32,35 @@ public class TestPorExtenso {
 		
 	}
 	
-	
-
+	@Test
+	public void testNumerosDeUmaPalavra(){
+		
+		String[] numeros = {"zero","um", "dois","tres", "quatro","cinco","seis","sete","oito"
+				,"nove", "dez","onze","doze", "treze", "catorze", "quinze", "dezesseis", "dezessete"
+				,"dezoito", "dezenove", "vinte","trinta","quarenta","cinquenta", "sessenta",
+				"setenta", "oitenta", "noventa", "cem","duzentos", "trezentos", "quatrocentos",
+				"quinhentos", "seiscentos", "setecentos","oitocentos", "novecentos", "mil" }; 
+		
+		
+		// Testa ate 20
+		for (int i = 11; i < 21; i++) {					
+			assertEquals(transformador.transformaExtenso(i+""), numeros[i]);
+		}
+		
+		//Testa de 30 ate 100
+		//37
+		int contador = 30;
+		for(int n = 21; n < 29; n++){
+			assertEquals(transformador.transformaExtenso(contador+""), numeros[n]);
+			contador += 10;
+		}
+		
+		// Testa ate 1000
+		int i = 29;
+		for(int n = 200; n <= 1000; n += 100){
+			assertEquals(transformador.transformaExtenso(n + ""), numeros[i]);
+			i += 1;
+		}		
+		
+	}	
 }
