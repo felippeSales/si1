28779cbf5c92b713325cc091f75d2696402ad0b0
Felipe Sales <sales.felipeb@gmail.com>
2013-11-24 20:20:21 -0300

    Rendering the first page

--- lab2/todoList/app/controllers/Application.java
+++ lab2/todoList/app/controllers/Application.java
@@ -1,14 +1,30 @@
 package controllers;
 
 import play.*;
-import play.mvc.*;
 
+import play.data.*;
+import play.mvc.*;
+import models.*;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	static Form<Task> taskForm = Form.form(Task.class);
+	
+	public static Result index() {
+		return ok(views.html.index.render(Task.all(), taskForm));
+	}
 
-    public static Result index() {
-        return ok("Relou Mundis!");
-    }
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		return TODO;
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
 
 }
