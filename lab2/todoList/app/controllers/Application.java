f191a0fb4487fbf6fb848da429e4cf8635ff4873
Felipe Sales <sales.felipeb@gmail.com>
2013-11-24 20:41:19 -0300

    Deleting Tasks

--- lab2/todoList/app/controllers/Application.java
+++ lab2/todoList/app/controllers/Application.java
@@ -8,9 +8,9 @@ import models.*;
 import views.html.*;
 
 public class Application extends Controller {
-	
+
 	static Form<Task> taskForm = Form.form(Task.class);
-	
+
 	public static Result index() {
 		return ok(views.html.index.render(Task.all(), taskForm));
 	}
@@ -20,19 +20,18 @@ public class Application extends Controller {
 	}
 
 	public static Result newTask() {
-		  Form<Task> filledForm = taskForm.bindFromRequest();
-		  if(filledForm.hasErrors()) {
-		    return badRequest(
-		      views.html.index.render(Task.all(), filledForm)
-		    );
-		  } else {
-		    Task.create(filledForm.get());
-		    return redirect(routes.Application.tasks());  
-		  }
+		Form<Task> filledForm = taskForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(Task.all(), filledForm));
+		} else {
+			Task.create(filledForm.get());
+			return redirect(routes.Application.tasks());
 		}
+	}
 
 	public static Result deleteTask(Long id) {
-		return TODO;
+		Task.delete(id);
+		return redirect(routes.Application.tasks());
 	}
 
 }
